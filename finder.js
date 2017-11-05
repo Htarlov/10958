a58024fd023df537a2daad4425c7a42dc81e2e2d
Zbyszek <zbyszek.matuszewski@gmail.com>
2017-11-05 09:39:46 +0100

    Refactorization, license info, description

--- finder.js
+++ finder.js
@@ -1,19 +1,28 @@
-const elements = ['','*','+','-','/','**','**-','/-','*-']; // operators, ** means ^
-const felements = ['', '-']; // operators before first element
+/**
+* Title: The 10958 problem solver
+* Description: JavaScript console script that searches for equations that equal or are most close to 10958 number
+*              and that consist of numbers 1, 2, ..., 9 in order and brackets and negations and operators: concatenation, *, +, -, /, ^
+* How to run: node finder.js
+* Author: Zbigniew Matuszewski <zbyszek.matuszewski@gmail.com>
+* License: MIT (https://opensource.org/licenses/mit-license.php)
+**/
+
+const operators = ['','*','+','-','/','**','**-','/-','*-']; // operators, ** means ^
+const foperators = ['', '-']; // operators before first element
 const target = 10958.0;
 const show_all = false; // to debug
 
-var best_str = '0';
+var best_equation = '0';
 var best_delta = 1*target;
 var best_value = 0;
 var delta = 1*target;
 var count = 0;
 
 // Runs after last level of reccurence reached and equation is nearly full (except last closing brackets)
-// str - current equation or it's part
+// equation - current equation or it's part
 // onum - number of opened brackets
 // opens - array of positions of opened brackets
-function finish_equation (str, onum, opens) {
+function finish_equation (equation, onum, opens) {
     // do not try to eveluate if there is ...((...)) as it is same as ...(...)
     for (var k=1; k<onum; k++) {
       if (opens[k] == opens[k-1]) {
@@ -21,29 +30,29 @@ function finish_equation (str, onum, opens) {
       }
     }
     // close all unclosed brackets
-    if (onum > 0) for (var n=0; n<onum; n++) str += ')';
+    if (onum > 0) for (var n=0; n<onum; n++) equation += ')';
     var fn = true;
     
     try {
-      v = eval(str);
+      v = eval(equation);
     } catch (e) {
       v = 0;
     }
     if (isNaN(v)) v = 0; // for case of division by zero
     delta = Math.abs(v - target)
     if (delta < best_delta) {
-      best_str = str;
+      best_equation = equation;
       best_delta = delta;
       best_value = v;
-      console.log('New best value '+v+' for equation '+str+' delta '+delta);
+      console.log('New best value '+v+' for equation '+equation+' delta '+delta);
     }
     count++;
     if (count % 10000000 == 0) {
-      console.log('Checked '+count+' possibilities. Last checked: '+str);
-      console.log('Best so far: '+best_str+' with value: '+best_value+' and delta: '+best_delta);
+      console.log('Checked '+count+' possibilities. Last checked: '+equation);
+      console.log('Best so far: '+best_equation+' with value: '+best_value+' and delta: '+best_delta);
     }
     if (show_all) {
-      console.log('Current: '+str+' value '+v+' delta: '+delta);
+      console.log('Current: '+equation+' value '+v+' delta: '+delta);
     }
 }
 
@@ -67,24 +76,24 @@ function count_same_and_before (onum, opens) {
 
 // Runs recurrently and checks on last level of recurrence
 // level - current number
-// str - current equation or it's part
+// equation - current equation or it's part
 // onum - number of opened brackets
 // opens - array of positions of opened brackets
 // just_opened - was bracket just opened (do not want ...+(67).. etc.)
-function check (level, str, onum, opens, just_opened) {
+function check (level, equation, onum, opens, just_opened) {
     // finish at level 10 - after goint through all 1-9 numbers
     if (level == 10) {
-        finish_equation (str, onum, opens); 
+        finish_equation (equation, onum, opens); 
         return;
     }
     // run when level < 10
     // for level 1 we start equation so we use only empty operator or negation, 
     // otherwise all possible elements
-    var els = level == 1 ? felements : elements;
-    for (var x in els) { // iterate through possible operators
-        var e = els[x]; // current operator
-        var st = ''; // starting brackets string
-        var en = ''; // ending brackets string
+    var ops = level == 1 ? foperators : operators;
+    for (var x in ops) { // iterate through possible operators
+        var operator = ops[x]; // current operator
+        var st_brackets = ''; // starting brackets
+        var en_brackets = ''; // ending brackets
         var a; // counter
         var max_st = 0; // maximal number of starting brackets (might be changed below)
         var min_en = 0; // minimal number of ending brackets
@@ -93,7 +102,7 @@ function check (level, str, onum, opens, just_opened) {
         var same_num = same_and_before[0];
         var before_num = same_and_before[1];
         
-        if (level == 1 || e != '') { // do not open on concatenations
+        if (level == 1 || operator != '') { // do not open on concatenations
           // we do not want to open more than we can reasonably close without ...((...))...
           max_st = 9 - level - same_num;
           if (max_st < 1 && level < 9) max_st = 1; 
@@ -106,37 +115,35 @@ function check (level, str, onum, opens, just_opened) {
         // also does not close if current operator is concatenation
         // maximal number of closes is at most number of all ( and at most up to one of (( 
         // so we are not going into ...((...))...
-        if (!just_opened && onum > 0 && e != '') max_en = before_num;
+        if (!just_opened && onum > 0 && operator != '') max_en = before_num;
             
         if (min_en > max_en) return; // no sense to go further, will surely have ...((...))... or ...((...)) soon
 
         for (var n_st=0; n_st<=max_st; n_st++) { 
           // iterate through possible numbers of starting brackets
-          st = '';
+          st_brackets = '';
           var max_m = (n_st > 1 || level == 1) ? 1 : 0; // 1 means that can add additional negation before brackets
           // add negation only on second bracket as for first it is already 
           // checked (operators '*-' etc.) except for starting entry where always allowed so cound have -(...
           for (var m=0; m <= max_m; m++) { // 0 - no negation, 1 - negation
-            if (n_st > 0) for (a = 0; a<n_st; a++) st += m == 0 ? '(' : '-(';
+            if (n_st > 0) for (a = 0; a<n_st; a++) st_brackets += m == 0 ? '(' : '-(';
             for (var n_en=min_en; n_en <= max_en; n_en++) { // iterate all possible numbers of ending brackets
-                en = '';
-                if (n_en > 0) for (a = 0; a<n_en; a++) en += ')';
+                en_brackets = '';
+                if (n_en > 0) for (a = 0; a<n_en; a++) en_brackets += ')';
                 var opens2 = []; // new array of positions of bracket opens
                 for (var i=0; i<opens.length - n_en; i++) opens2.push(opens[i]); // copy not closed ones
                 for (var j=0; j<n_st; j++) opens2.push(level); // add new ones
-                var str2 = str + en + e + st + level; // add fragment of equation
+                var equation2 = equation + en_brackets + operator + st_brackets + level; // add fragment of equation
                 var onum2 = 1*onum + n_st - n_en; // recalc number of brackets
                 // previous value if concatenated, otherwise check if started a bracket
-                var just_opened2 = e.length == 0 ? just_opened : n_st > 0; 
-                check (level+1, str2, onum2, opens2, just_opened2)
+                var just_opened2 = operator.length == 0 ? just_opened : n_st > 0; 
+                check (level+1, equation2, onum2, opens2, just_opened2)
             }
           }
         }
     }
 }
 
-
-str = ''
-check (1, str, 0, [], false);
+check (1, '', 0, [], false);
 
 console.log('Best value '+best_value+' for equation '+ best_str+' delta '+ best_delta);
